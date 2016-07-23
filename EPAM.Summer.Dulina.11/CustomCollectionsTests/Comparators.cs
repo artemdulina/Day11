82abba97a1584fcf593e4ab2220afdd3125840b3
Артем <@->
2016-07-23 22:09:13 +0300

    initial commmit. Alpha BinaryTree<T>

---
+++ EPAM.Summer.Dulina.11/CustomCollectionsTests/Comparators.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomCollectionsTests
+{
+    public class CompareByValue : IComparer<int>
+    {
+        public int Compare(int x, int y)
+        {
+            if (x > y)
+                return -1;
+            if (x < y)
+                return 1;
+            return 0;
+        }
+    }
+
+    public class CompareStrings : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            if (x.Length > y.Length)
+                return 1;
+            if (x.Length < y.Length)
+                return -1;
+            return 0;
+        }
+    }
+
+    //Критерий сравнения точек?
+    public class ComparePoints : IComparer<Point>
+    {
+        public int Compare(Point x, Point y)
+        {
+            if (x.X > y.X)
+                return 1;
+            if (x.X < y.X)
+                return -1;
+            return 0;
+        }
+    }
+}
