2dd0c32b6ec7443edce08afb396ea025f5054047
Артем <@->
2016-07-24 13:30:54 +0300

    add alpha version of SquareMatrix and event

---
+++ EPAM.Summer.Dulina.11/MathematicalEntities/IMatrix.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathematicalEntities
+{
+    public interface IMatrix<TValue> : IEnumerable<TValue>
+    {
+        TValue this[int nIndex, int mIndex] { get; set; }
+        int N { get; }
+        int M { get; }
+        IMatrix<TValue> Multiply(IMatrix<TValue> matrix);
+        IMatrix<TValue> Multiply(TValue value);
+        IMatrix<TValue> Difference(IMatrix<TValue> matrix);
+        IMatrix<TValue> Difference(TValue value);
+    }
+}
---
+++ EPAM.Summer.Dulina.11/MathematicalEntities/IndexChangeEventArgs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathematicalEntities
+{
+    public sealed class IndexChangeEventArgs<T>
+    {
+        public int I { get; }
+        public int J { get; }
+        public T OldValue { get; }
+        public T NewValue { get; }
+
+        public IndexChangeEventArgs(int i, int j, T oldValue, T newValue)
+        {
+            I = i;
+            J = j;
+            OldValue = oldValue;
+            NewValue = newValue;
+        }
+    }
+}
